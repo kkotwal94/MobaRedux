0801bd00171e916139b74244b8fe83fe0f198e3a
kkotwal94 <bNastyK@udel.edu>
2016-07-28 21:04:49 -0400

    Added in material ui and tap event, along with about page minor edit

--- app/containers/About.jsx
+++ app/containers/About.jsx
@@ -12,18 +12,18 @@ const cx = classNames.bind(styles);
 const About = () => {
   return (
     <div className={cx('about')}>
-      <h1 className={cx('header')}>About Ninja Ocean</h1>
+      <h1 className={cx('header')}>About MobaRedux</h1>
       <div className={cx('description')}>
-        <p>Imagine an ocean of ninjas. Now think of it as a metaphor.<br />
-          Seriously, we love good tech. React, redux, scala, Haskell, machine learning, you name it!
+        <p>Make your own moba heroes and troll<br />
+          Seriously, I love all dem mobas and being able to get stats for it, were all tryna get better right?!
         </p>
       </div>
       <div className={cx('contribute')}>
         <p>Want to contribute? Help us out!
           If you think the code on &nbsp;
-          <a target="_blank" href="https://github.com/choonkending/react-webpack-node">this repo</a>
+          <a target="_blank" href="https://github.com/kkotwal94/MobaRedux">this repo</a>
         &nbsp;could be improved, please create an issue&nbsp;
-          <a target="_blank" href="https://github.com/choonkending/react-webpack-node/issues">here</a>!
+          <a target="_blank" href="https://github.com/kkotwal94/MobaRedux/issues">here</a>!
         </p>
       </div>
     </div>
