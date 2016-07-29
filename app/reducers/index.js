1b1df949b8069cccceb704e0142ffe9884b3a79e
kkotwal94 <bNastyK@udel.edu>
2016-07-28 20:43:34 -0400

    Removed unecessary docs, reducers, actions, and testcases

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import user from 'reducers/user';
-import topic from 'reducers/topic';
 import message from 'reducers/message';
 import { routerReducer as routing } from 'react-router-redux';
 
@@ -8,7 +7,6 @@ import { routerReducer as routing } from 'react-router-redux';
 // router state
 const rootReducer = combineReducers({
   user,
-  topic,
   message,
   routing
 });
